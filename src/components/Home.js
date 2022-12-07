fb9ff6f274b7a9f4df1f19a2eaea68a55a34bb29
boualem06 <kb_hamroune@esi.dz>
2022-12-08 00:03:38 +0100

    adding the speakers page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,9 @@ const Home = () => {
             <div style={{ height: "100vh" }} className="w-full  flex justify-center lg:justify-start items-center my-auto">
                 <div className="lg:px-10 ">
                     <h1 className="text-white text-6xl lg:text-8xl text-center mb-10 font-bold ">Electro-Fest</h1>
-                    <h1 className="text-white text-6xl lg:text-8xl font-bold ">MM/DD-MM/DD</h1>
+                    <h1 className="text-white text-6xl lg:text-8xl font-bold mt-4">December 22-23-24th 2022</h1>
+                    <h1 className="text-white text-6xl lg:text-8xl font-bold mt-4">9:00 AM</h1>
+                    <button className="px-4 font-bold text-xl text-white mt-4 bg-[#FFD60B] py-1 rounded-full border-2 border-white md:py-4 md:px-8">Register Now</button>
                 </div>
 
             </div>
