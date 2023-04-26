c29639926d1a034fc82c7027787226daf71f6532
boualem06 <kb_hamroune@esi.dz>
2023-04-26 22:33:07 +0100

    integrating the login and register page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,41 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import pic from "../assets/images/login.png"
 
 const Register = () => {
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: ""
+    })
+    const [error,setError]=useState({})
+    const submit = () => {
+        console.log(userInfo)
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!regex.test(userInfo.email)) {
+            setError({message:"invalide email"});
+        }else{
+        fetch('http://127.0.0.1:5000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userInfo)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.status===400){
+                    setError({message:data.message})
+                }
+                else{
+                    setError({})
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+    }
     return (
         <div className=' flex justify-center items-center md:h-[40rem] mb-10 md:mb-0 px-2 '>
             <div style={{ boxShadow: "0 0 20px rgba(0, 0, 0, 0.5)" }} className="grid sm:grid-cols-2 grid-cols-1  rounded-lg ">
@@ -9,12 +43,14 @@ const Register = () => {
                 <div className="bg-[#faa935] w-full text-center py-4 px-10 lg:px-14">
                     <div>
                         <h1 className="text-white font-bold text-3xl">Login</h1>
-                        <input className="rounded-md px-2 py-1 mt-16 w-full" placeholder="your Email... "></input>
-                        <input type="password" className="rounded-md px-2 py-1 mt-10 w-full" placeholder="your password... "></input>
+                        <input onChange={(e) => { setUserInfo({ ...userInfo, email: e.target.value }) }} className="rounded-md px-2 py-1 mt-16 w-full" placeholder="your Email... "></input>
+                        <input onChange={(e) => { setUserInfo({ ...userInfo, password: e.target.value }) }} type="password" className="rounded-md px-2 py-1 mt-10 w-full" placeholder="your password... "></input>
                     </div>
 
-                    <button className="bg-black text-white mt-16 w-full px-2 py-1 rounded-md font-bold hover:cursor-pointer hover:shadow-md">Login</button>
+                    <button onClick={submit} className="bg-black text-white mt-16 w-full px-2 py-1 rounded-md font-bold hover:cursor-pointer hover:shadow-md">Login</button>
                     <h1 className="text-white text-sm mt-6 font-bold">Don't have an account ? <Link to={"/register"}><span className="font-bold text-black hover:underline hover:cursor-pointer">Register</span></Link></h1>
+                    {error && <h1 className="text-red-500 text-sm mt-6 font-bold">{error.message}</h1>}
+
                 </div>
             </div>
         </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,41 @@
 import { Link } from "react-router-dom"
 import pic from "../assets/images/login.png"
-
+import { useState } from "react"
 const Register = () => {
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+        name:""
+    })
+    const [error,setError]=useState({})
+    const submit = () => {
+        console.log(userInfo)
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!regex.test(userInfo.email)) {
+            setError({message:"invalide email"});
+        }else{
+        fetch('http://127.0.0.1:5000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userInfo)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.status===400){
+                    setError({message:data.message})
+                }
+                else{
+                    setError({})
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        }
+    }
     return (
         <div className=' flex justify-center items-center md:h-[40rem] mb-10 md:mb-0 px-2 '>
         <div style={{ boxShadow: "0 0 20px rgba(0, 0, 0, 0.5)" }} className="grid sm:grid-cols-2 grid-cols-1  rounded-lg ">
@@ -9,13 +43,15 @@ const Register = () => {
             <div className="bg-[#faa935] w-full text-center py-4 px-10 lg:px-14">
                 <div>
                     <h1 className="text-white font-bold text-3xl">Register</h1>
-                    <input className="rounded-md px-2 py-1 mt-16 w-full" placeholder="your Email... "></input>
-                    <input className="rounded-md px-2 py-1 mt-16 w-full" placeholder="your Name... "></input>
-                    <input type="password" className="rounded-md px-2 py-1 mt-10 w-full" placeholder="your password... "></input>
+                    <input onChange={(e) => { setUserInfo({ ...userInfo, email: e.target.value }) }} className="rounded-md px-2 py-1 mt-16 w-full" placeholder="your Email... "></input>
+                    <input onChange={(e) => { setUserInfo({ ...userInfo, name: e.target.value }) }} className="rounded-md px-2 py-1 mt-16 w-full" placeholder="your Name... "></input>
+                    <input onChange={(e) => { setUserInfo({ ...userInfo, password: e.target.value }) }} type="password" className="rounded-md px-2 py-1 mt-10 w-full" placeholder="your password... "></input>
                 </div>
 
-                <button className="bg-black text-white mt-16 w-full px-2 py-1 rounded-md font-bold hover:cursor-pointer hover:shadow-md">register</button>
+                <button onClick={submit} className="bg-black text-white mt-16 w-full px-2 py-1 rounded-md font-bold hover:cursor-pointer hover:shadow-md">register</button>
                 <h1 className="text-white text-sm mt-6 font-bold">Already have an account ? <Link to={"/login"}><span className="font-bold text-black hover:underline hover:cursor-pointer">login</span></Link></h1>
+                {error && <h1 className="text-red-500 text-sm mt-6 font-bold">{error.message}</h1>}
+
             </div>
         </div>
     </div>
@@ -25,23 +61,3 @@ const Register = () => {
 export default Register
 
 
-{/*  */}
-
-
-
-{/* <div className=' flex justify-center items-center md:h-[40rem] mb-10 md:mb-0 px-2 '>
-<div style={{ boxShadow: "0 0 20px rgba(0, 0, 0, 0.5)" }} className="grid sm:grid-cols-2 grid-cols-1  rounded-lg ">
-    <img className="hidden sm:block text-center" style={{ maxHeight: "30rem", maxWidth: "25rem" }} src={pic} alt="login"></img>
-    <div className="bg-[#faa935] w-full text-center py-4 px-10 lg:px-14">
-        <div>
-            <h1 className="text-white font-bold text-3xl">Register</h1>
-            <input className="rounded-md px-2 py-1 mt-16 w-full" placeholder="your Name... "></input>
-            <input className="rounded-md px-2 py-1 mt-10 w-full" placeholder="your Email... "></input>
-            <input type="password" className="rounded-md px-2 py-1 mt-10 w-full" placeholder="your password... "></input>
-        </div>
-
-        <button className="bg-black text-white mt-16 w-full px-2 py-1 rounded-md font-bold hover:cursor-pointer hover:shadow-md">Login</button>
-        <h1 className="text-white text-sm mt-6 font-bold">already have an account ? <Link to={"/login"}><span className="font-bold text-black hover:underline hover:cursor-pointer">Login</span> </Link></h1>
-    </div>
-</div>
-</div> */}
