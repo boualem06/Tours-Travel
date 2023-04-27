a419b3afd2d6b44e8c0e8e5c5eb05ef58bb6aac4
boualem06 <kb_hamroune@esi.dz>
2023-04-27 09:31:42 +0100

    integrating the featured tours

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import tour1 from "../assets/images/tour-img01.jpg"
 // LocationOn
 import LocationOn from '@material-ui/icons/LocationOn';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 // import { makeStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
 
@@ -10,12 +10,14 @@ import Rating from '@material-ui/lab/Rating';
 
 
 
-const Card = () => {
+const Card = (props) => {
 
     const [value, setValue] = React.useState(2);
     const [hover, setHover] = React.useState(-1);
     // const classes = useStyles();
-
+    useEffect(()=>{
+        console.log(props.tour._id)
+    },[])
     return (
         <div className="rounded-md border-2 border w-fit shadow-lg">
             <img style={{ height: "15rem" }} src={tour1} alt="Tour" className="rounded-md"></img>
@@ -23,7 +25,7 @@ const Card = () => {
                 <div className="flex items-center justify-between">
                     <div className="flex text-[#faa935]">
                         <LocationOn></LocationOn>
-                        <h1 className="font-bold">London</h1>
+                        <h1 className="font-bold">{props.tour.address}</h1>
                     </div>
                     <div>
                         <Rating
@@ -33,10 +35,10 @@ const Card = () => {
                     </div>
                 </div>
 
-                <h1 className="font-bold mt-2 text-lg">Washington Bridge</h1>
+                <h1 className="font-bold mt-2 text-lg">{props.tour.city}</h1>
 
                 <div className=" mt-2 flex items-center justify-between w-full">
-                    <h1><span className="text-[#faa935] font-bold">$99</span>/per person</h1>
+                    <h1><span className="text-[#faa935] font-bold">${props.tour.price}</span>/per person</h1>
                     <button className="px-2 bg-[#faa935] text-white py-1 rounded-md">Book Now</button>
                 </div>
             </div>
--- src/pages/FeaturedTours.js
+++ src/pages/FeaturedTours.js
@@ -1,18 +1,40 @@
+import { useEffect, useState } from "react";
 import Card from "../components/Card"
 const FeaturedTours = () => {
+    const [featuredTours, setFeaturedTours] = useState([])
+
+    useEffect(() => {
+        const fetchingFeaturedTours = async () => {
+            let headersList = {
+                "Accept": "*/*",
+                "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+                "accestoken": localStorage.getItem("token")
+            }
+
+            let response = await fetch("http://localhost:5000/getFeaturedTours", {
+                method: "GET",
+                headers: headersList
+            });
+
+            let data = await response.json();
+            setFeaturedTours(data.toursArray)
+        }
+
+        fetchingFeaturedTours()
+
+    }, [])
+
+
+
     return (
         <div>
             <h1 className="font-bold text-3xl ">Our Featured Tours</h1>
             <div className="flex justify-center mt-4 lg:px-4 md:px-3 sm:px-2 px-1">
                 <div className="grid md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-10 md:gap-5 sm:gap-4 gap-4">
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
+                    {featuredTours.map((elem) => {
+                        return (<Card tour={elem} ></Card>)
+                    }
+                    )}
 
                 </div>
             </div>
